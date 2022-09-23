dece4c2ad88078c28d86b72ff00556fd1c6b4305
CHUN <104182883+ChunSeungJun@users.noreply.github.com>
2022-09-23 14:39:35 +0900

    WindowAPI

---
+++ Windows/Windows/Window.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+// WINAPI
+// Windows		: Operating System Window OS
+// Application	: 응용 프로그램
+// Programming	:
+// Interface	: 함수의 집합체
+
+// Windows 가 제공하는 함수의 집합체로
+// 응용 프로그램을 프로그래밍하는 언어
+
+int WAPIENTRY inMain() 
+{
+	
+	return 0;
+}
